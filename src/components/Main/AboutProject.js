77c044fb296c68d4aa73b2170d5f6c742a9bb701
Alexandr Andreev <awa3awa@yandex.ru>
2022-11-07 16:47:12 +0700

    Temporary

---
+++ src/components/Main/AboutProject.js
@@ -0,0 +1,48 @@
+
+import React from 'react';
+
+import './AboutProject.css';
+
+import NavTab from './NavTab';
+
+function AboutProject(props) {
+
+    return (
+        <section className='about'>
+            <NavTab text='О проекте'/>
+
+            <div className='about__wrapper'>
+                <article className='about__article'>
+                    <h4 className='about__title'>Дипломный проект включал 5 этапов</h4>
+                    <p className='about__text'>
+                        Составление плана, работу над бэкендом,
+                        вёрстку, добавление функциональности и
+                        финальные доработки.
+                    </p>
+                </article>
+
+                <article className='about__article'>
+                    <h4 className='about__title'>На выполнение диплома ушло 5 недель</h4>
+                    <p className='about__text'>
+                        У каждого этапа был мягкий и жёсткий дедлайн,
+                        которые нужно было соблюдать, чтобы успешно
+                        защититься.
+                    </p>
+                </article>
+            </div>
+
+            <ul className='about__steps'>
+                <li className='about__steps_week'>
+                    <p className='about__steps_week-item'>1 неделя</p>
+                    <p className='about__steps_week-item'>4 недели</p>
+                </li>
+                <li className='about__steps_tech'>
+                    <p className='about__steps_tech-item'>Back-end</p>
+                    <p className='about__steps_tech-item'>Front-end</p>
+                </li>
+            </ul>
+        </section>
+    );
+}
+
+export default AboutProject;
