c8b669c22cd916b5f9d919fff2ae94bf945fc78c
Alexandr Andreev <awa3awa@yandex.ru>
2022-11-09 16:07:46 +0700

    hero

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './Main.css';
 import About from './About'
+import Hero from './Hero';
 
 function Main(props) {
 
     return (
+        <div>
+        <Hero />
         <About />
-
+        </div>
     );
 }
 
