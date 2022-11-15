ef24aa9185607d8121072b7a67064864befb2ef7
Alexandr Andreev <awa3awa@yandex.ru>
2022-11-15 08:36:13 +0700

    temp

--- src/components/movies/MoviesCard.js
+++ src/components/movies/MoviesCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import {useHistory} from 'react-router-dom';
 import imagesCard from '../../images/zim.jpg';
 import './MoviesCard.css';
 
 
 function MoviesCard() {
     const [isSaved, setIsSaved] = React.useState(false);
+    const history = useHistory();
 
     const handleSaved = () =>{
         setIsSaved(!isSaved);
@@ -19,7 +20,7 @@ function MoviesCard() {
                     <h2 className='card__title'>Название фильма</h2>
                     <p className='card__subtitle'>6:66</p>
                 </div>
-                <button onClick={handleSaved} className={isSaved ? 'card__button card__button-save' : 'card__button card__button-not-save'}/>
+                <button onClick={handleSaved} className={(isSaved ? 'card__button card__button-save' : 'card__button card__button-not-save')+  (history.location.pathname === '/saved-movies' ? 'card__button card__button-delete' : '')}/>
             </header>
 
         </article>
