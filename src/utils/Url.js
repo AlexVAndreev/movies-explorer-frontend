ccb9591b59c5eed01d8fb63d9dd4e1595e3e1b41
Alexandr Andreev <awa3awa@yandex.ru>
2022-11-20 11:06:21 +0700

    temp

--- src/utils/Url.js
+++ src/utils/Url.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "https://awaback.nomoredomains.icu";
-// export const BASE_URL = "http://localhost:3001";
+// export const BASE_URL = "https://awaback.nomoredomains.icu";
+
+export const BASE_URL = "http://84.201.160.234";
 export const MOVIE_URL = "https://api.nomoreparties.co";
